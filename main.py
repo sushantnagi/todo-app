fb41be686c29b9115ca021ebeca5e8dfa17ea9d5
lizardanon666i <sushantnagi669@gmail.com>
2024-07-09 18:49:51 +0530

    Day 6 101

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-todos = []
+# If we input show after running the program, it outputs error as 'todos' has not been defined
 while True:
     user_input = input('Enter add, show, edit, completed or exit: ')
     user_input = user_input.strip()
+    user_input = user_input.lower()
     match user_input:
         case 'add':
-            task = input('Add a task: ')
-            todos.append(task)
+
+            task = input('Add a task: ') + "\n"
+
+            file = open('Files/TDL.txt', 'r')  # Opens the file and reads it
+            todos = file.readlines()  # add the content of the file to todos list. If file is empty, nothing is added
+            file.close()
+
+            todos.append(task)  # input task is added tp the list
+
+            file = open('Files/TDL.txt', 'w')
+            file.writelines(todos)  # list is reprinted on the file
+            file.close()
+
         case 'show' | 'display':
+            fileshow = open('Files/TDL.txt', 'r')
+            todos = fileshow.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 item = item.title()
                 index = index + 1
@@ -25,7 +41,10 @@ while True:
             number = int(input('Enter task number to mark completed: '))
             todos.pop(number - 1)
 
+        case 'newfile':
+            filenew = open('whatever.txt', 'w')
+            file.close()
+
         case random_input:
             print('Please enter the correct input')
-
 print('Thanks')
